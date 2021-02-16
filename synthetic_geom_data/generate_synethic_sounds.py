2d7022b89aeec7aee7b35e23c09083259920d98c
imandel <im334@cornell.edu>
2021-02-15 21:59:01 -0500

    sounds distributed across uberH3 res 10

--- synthetic_geom_data/generate_synethic_sounds.py
+++ synthetic_geom_data/generate_synethic_sounds.py
@@ -2,31 +2,62 @@ import pandas as pd
 from gtts import gTTS
 import pickle as pk
 import lorem
-
-raw_data = pd.read_csv('open_streets_locations.csv')
-
-flatten = lambda t: [item for sublist in t for item in sublist]
-
-def parse_latlongstr(raw_str):
-    start_i = raw_str.find("((")
-    end_i = raw_str.find("))")
-    multiline_str = raw_str[start_i+2:end_i]
-    latlong_strs = multiline_str.split(",")
-    return latlong_strs
+import json
+import urllib.request
+import geojson
+import h3
+import time
+from pathlib import Path
+from tqdm import tqdm
 
 
-geom_data = raw_data['the_geom']
-extracted_latlongstrs = [parse_latlongstr(raw_str) for raw_str in geom_data.tolist()]
-flattened_lat_longstrs = flatten(extracted_latlongstrs)
+sounds = Path('./sounds')
+sounds.mkdir(parents=True, exist_ok=True)
 
-dataset_map_key = {}
+with urllib.request.urlopen('https://data.cityofnewyork.us/resource/7t3b-ywvw.geojson') as url:
+    boro = json.loads(url.read().decode())
+for b in boro['features']:
+    for pol in b['geometry']['coordinates']:
+        for segment in pol:
+            for idx, point in enumerate(segment):
+                segment[idx] = point[::-1]
+centers = []
+for b in boro['features']:
+    for pol in b['geometry']['coordinates']:
+        points = map(h3.h3_to_geo, h3.polyfill(dict(geojson.Polygon(pol)), 10))
+        centers.extend(points)
 
-for latlongstr in flattened_lat_longstrs:
-    random_lorem_ipsum_txt = lorem.sentence()
-    text_for_speech = f'{random_lorem_ipsum_txt} {latlongstr}'
-    tts = gTTS(text_for_speech)
-    filename = hash(latlongstr)
-    dataset_map_key[filename] = latlongstr
+# raw_data = pd.read_csv('open_streets_locations.csv')
 
-with open('fp_latlong_map.pk', 'wb+') as outfile:
-    pk.dump(dataset_map_key, outfile)
+# flatten = lambda t: [item for sublist in t for item in sublist]
+
+# def parse_latlongstr(raw_str):
+#     start_i = raw_str.find("((")
+#     end_i = raw_str.find("))")
+#     multiline_str = raw_str[start_i+2:end_i]
+#     latlong_strs = multiline_str.split(",")
+#     return latlong_strs
+
+
+# geom_data = raw_data['the_geom']
+# extracted_latlongstrs = [parse_latlongstr(raw_str) for raw_str in geom_data.tolist()]
+# flattened_lat_longstrs = flatten(extracted_latlongstrs)
+
+
+with open('tts.csv', 'w') as of:
+    of.write('lat lng filename\n')
+    for latlong in tqdm(centers):
+        random_lorem_ipsum_txt = lorem.sentence()
+        latlongstr = f"{latlong[0]} {latlong[1]}"
+        text_for_speech = f"{random_lorem_ipsum_txt} {latlongstr}"
+        filename = hash(latlongstr)
+        of.write(f"{latlongstr} {filename}\n")
+        if Path(sounds / f"{filename}.mp3").exists():
+            continue
+        tts = gTTS(text_for_speech)
+        tts.save(sounds / f"{filename}.mp3")
+        time.sleep(1)
+        # dataset_map_key[filename] = latlongstr
+
+# with open('fp_latlong_map.pk', 'wb+') as outfile:
+#     pk.dump(dataset_map_key, outfile)
