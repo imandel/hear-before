4d2185b558a24bb37f3fac7416ec9cc7142f8eba
Amrit Kwatra <amrit.kwatra@enigma.com>
2021-02-28 23:51:45 -0500

    switching to howler

--- index.js
+++ index.js
@@ -2,24 +2,37 @@ import mapboxgl from 'mapbox-gl';
 import { point } from '@turf/helpers';
 import distance from '@turf/distance';
 import { RecordingToggle } from './recording';
+import {Howl, Howler} from 'howler';
+
+Howl.prototype.changeSrc = function (newSrc) {
+  let self = this;
+  self.unload();
+  self._src = newSrc;
+  self.load();
+}
 
 const numAudioNodes = 10;
 const audioNodes = [];
 let gps;
 
 for (let i = 0; i < numAudioNodes; i++) {
-  const node = new Audio();
-  node.volume = 0;
-  // testing silence fix
-  node.src = 'https://hear-before-nyc.s3.amazonaws.com/sounds/1sSilent.mp3';
-  node.onended = () => { setTimeout(() => { node.play(); }, 2000); };
+  const node = new Howl({
+    src: ['https://hear-before-nyc.s3.amazonaws.com/audiobook_sounds/knickerbockershistorynyvol1/knickerbockershistorynyvol1_chpt_6_segment_5.mp3'],
+    autoplay: true,
+    loop: false,
+    volume: 0,
+    // onend: function() {
+    //   setTimeout(() => { node.play(); }, 2000); 
+    // }
+  }
+  );
   audioNodes.push(node);
 }
 
 setInterval(() => {
   audioNodes.forEach((node) => {
     if (node.volume!== 0) {
-      console.log('src: ', node.src, ', vol:', node.volume);
+      // console.log('src: ', node.src, ', vol:', node.volume);
     }
   });
 }, 500);
@@ -33,7 +46,7 @@ window.audio = audio;
 const testpoint = point([-73.95630, 40.75617]);
 
 // testing easeinCirc
-const scaleAudio = (vol) => 1 - Math.sqrt(1 - vol ** 2);
+const scaleAudio = (vol) => 1.0 - (1 - vol ** 2)**0.5;
 
 // dist to audio source
 // audioStart is distance you start hearing anything
@@ -87,18 +100,21 @@ const rankAudios = () => {
 geolocate.on('geolocate', (e) => {
   const { latitude, longitude } = e.coords;
   gps = point([longitude, latitude]);
-
+  console.log('version 1.2 attempt to switch to howler')
   const closeTen = rankAudios().slice(0, 10);
+  console.log(closeTen);
   audioNodes.forEach((node, idx) => {
     const rankSrc = `https://hear-before-nyc.s3.amazonaws.com/${closeTen[idx].properties.filename}`
+    console.log(rankSrc);
     if(node.src !== rankSrc && node.volume===0){
-      node.src = rankSrc;
-      node.volume = 0;
-      node.oncanplaythrough = () => node.play();
+      node.changeSrc(rankSrc);
+      node.once('load', node.play);
     }
     // audioNodes[idx].src = `./sounds/${soundFeatures.properties.filename}`;
     const dist = distance(gps, point([closeTen[idx].properties.lng, closeTen[idx].properties.lat]));
-    node.volume = dist2volume(dist, 0.07);
+    node.volume(dist2volume(dist, 0.07)+0.01);
+
+    console.log(node);
   });
   // console.log(closeTen);
 });
