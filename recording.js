6165ba1ee960239b2c97b637a741850e8dacfae7
imandel <im334@cornell.edu>
2021-03-03 02:43:23 -0500

    uploads

--- recording.js
+++ recording.js
@@ -4,11 +4,15 @@ import MicrophonePlugin from 'wavesurfer.js/src/plugin/microphone';
 import { CognitoIdentityClient } from '@aws-sdk/client-cognito-identity';
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 import { fromCognitoIdentityPool } from '@aws-sdk/credential-provider-cognito-identity';
-import { Howl, Howler } from 'howler';
+import mime from 'mime';
+import { v4 as uuid } from 'uuid';
 
 const REGION = 'us-east-1';
 const BUCKET = 'hear-before-nyc';
 let streamID;
+let audioURI;
+let audioBlob;
+let type;
 
 const s3 = new S3Client({
   region: REGION,
@@ -18,36 +22,35 @@ const s3 = new S3Client({
   }),
 });
 
-const uploadFile = async () => {
+const uploadFile = async (blob, name, meta) => {
   const uploadParams = {
     Bucket: BUCKET,
-    Key: 'uploads/test.txt',
-    Body: 'this is a test pls',
+    Key: `uploads/${name}`,
+    Body: blob,
+    Metadata: meta,
   };
 
   try {
-    const data = await s3.send(new PutObjectCommand(uploadParams));
-    alert('that worked');
+    await s3.send(new PutObjectCommand(uploadParams));
   } catch (err) {
-    return alert('There was an error uploading file: ', err.message);
+    console.log('There was an error uploading file: ', err.message);
   }
 };
 
 // TODO mute all audionodes before recording/showing modal
 
-const downloadAudio = (url) => {
-  const a = document.createElement('a');
-  document.body.appendChild(a);
-  a.style = 'display: none';
-  a.href = url;
-  a.download = 'audio.mp4';
-  a.click();
-};
+// for testing
+// const downloadAudio = (url) => {
+//   const a = document.createElement('a');
+//   document.body.appendChild(a);
+//   a.style = 'display: none';
+//   a.href = url;
+//   a.download = 'audio.mp4';
+//   a.click();
+// };
 
-const setupAudio = (start, play, download, wave) => {
+const setupAudio = (start, play, upload, wave) => {
   play.disabled = true;
-  download.disabled = true;
-  let audio;
 
   if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     console.log('getUserMedia supported.');
@@ -80,30 +83,18 @@ const setupAudio = (start, play, download, wave) => {
           chunks.push(e.data);
         };
 
-        mediaRecorder.onstart = (e) => console.log('started mr');
-
         mediaRecorder.onstop = () => {
-          console.log('stopped');
-          const blob = new Blob(chunks);
-          // {
-          //   type: 'audio/mp4',
-          // });
+          type = mime.extension(mediaRecorder.mimeType);
+          audioBlob = new Blob(chunks);
+
           chunks = [];
-          const url = URL.createObjectURL(blob);
-
-          download.onclick = () => downloadAudio(url);
-          download.disabled = false;
-          // audio = new Audio(url);
-          console.log(url);
+          upload.disabled = false;
           const reader = new FileReader();
-          reader.readAsDataURL(blob);
+          reader.readAsDataURL(audioBlob);
           reader.onloadend = () => {
-            const base64data = reader.result;
-            audio = new Howl({
-              src: [base64data],
-            });
-
-            wave.load(base64data);
+            audioURI = reader.result;
+            wave.load(audioURI);
+            document.querySelector('#modal-1 > div > div > footer > textarea').style.display = 'block';
           };
         };
       })
@@ -121,19 +112,49 @@ class RecordingToggle {
     const record = document.querySelector('#modal-1 > div > div > footer > button:nth-child(1)');
     const play = document.querySelector('#modal-1 > div > div > footer > button:nth-child(2)');
     const localInput = document.querySelector('#file-input');
-    const download = document.querySelector('#modal-1 > div > div > footer > button:nth-child(3)');
+    const upload = document.querySelector('#modal-1 > div > div > footer > button:nth-child(3)');
+    const textarea = document.querySelector('#modal-1 > div > div > footer > textarea');
     let wavesurfer;
+    let lat; let
+      lng;
+    upload.onclick = () => {
+      uploadFile(audioBlob, `${uuid()}.${type}`,
+        // { lat: lat, lng: lng, comments: textarea.value}
+        { key: 'val' });
+      wavesurfer.empty();
+      upload.disabled = true;
+      play.disabled = true;
+    };
+
+    localInput.onchange = (e) => {
+      if (localInput.files.length > 0) {
+        audioBlob = localInput.files[0];
+        type = mime.extension(audioBlob.type);
+        const reader = new FileReader();
+        reader.readAsDataURL(audioBlob);
+        reader.onloadend = () => {
+          audioURI = reader.result;
+          wavesurfer.load(audioURI);
+          play.disabled = false;
+          upload.disabled = false;
+          document.querySelector('#modal-1 > div > div > footer > textarea').style.display = 'block';
+        };
+      }
+    };
     this._btn = document.createElement('button');
     this._btn.className = 'mapboxgl-ctrl-icon mapboxgl-ctrl-rec';
     this._btn.type = 'button';
     this._btn['aria-label'] = 'Toggle Recording';
     MicroModal.init();
     this._btn.onclick = () => {
-      // console.log('trying upload');
-      // uploadFile();
       if (!_this._btn.classList.contains('recording')) {
         _this._btn.classList.toggle('recording');
-        console.log('recording');
+        // this is super delayed and I'm not sure why
+        navigator.geolocation.getCurrentPosition((position) => {
+          console.log('recording got position');
+          lat = position.coords.latitude;
+          lng = position.coords.longitude;
+        });
         MicroModal.show('modal-1', {
           onClose() {
             _this._btn.classList.toggle('recording');
@@ -141,6 +162,7 @@ class RecordingToggle {
             streamID.getTracks().forEach((track) => {
               track.stop();
             });
+            upload.disabled = true;
           },
         });
         wavesurfer = WaveSurfer.create({
@@ -157,24 +179,10 @@ class RecordingToggle {
           ],
         });
         play.onclick = () => {
-            console.log('playing');
-            wavesurfer.play();
-          };
-        setupAudio(record, play, download, wavesurfer);
-        localInput.onchange = (e) => {
-          if (localInput.files > 0) {
-            console.log('selected file')
-            const file = localInput.files[0];
-
-            const reader = new FileReader();
-            reader.readAsDataURL(file);
-            reader.onloadend = () => {
-              const data = reader.result;
-              wavesurfer.load(data);
-              play.disabled = false;
-            };
-          }
+          console.log('playing');
+          wavesurfer.play();
         };
+        setupAudio(record, play, upload, wavesurfer);
       } else {
         MicroModal.close('modal-1');
       }
